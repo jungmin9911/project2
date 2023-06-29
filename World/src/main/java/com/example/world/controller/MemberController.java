bca9844c8e75357ff822aaaba0cafa23ff403659
jungmin9911 <jmahn1819@gmail.com>
2023-06-29 10:56:14 +0900

    22222

--- World/src/main/java/com/example/world/controller/MemberController.java
+++ World/src/main/java/com/example/world/controller/MemberController.java
@@ -3,11 +3,12 @@ package com.example.world.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
-import com.ezen.AmusementPark.service.MemberService;
+import com.example.world.service.MemberService;
 
 @Controller
 public class MemberController {
 
 	@Autowired
 	MemberService ms;
+	
 }
--- World/src/main/java/com/example/world/controller/NoticeController.java
+++ World/src/main/java/com/example/world/controller/NoticeController.java
@@ -2,12 +2,19 @@ package com.example.world.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.ezen.AmusementPark.service.NoticeService;
+import com.example.world.service.NoticeService;
 
 @Controller
 public class NoticeController {
 
 	@Autowired
 	NoticeService ns;
+	
+	@RequestMapping("/notice")
+	public String notice() {
+		return "qna/qna";
+	}
+	
 }
